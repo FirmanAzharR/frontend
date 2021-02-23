986e461e4fb4ac80203db5a842e96963ff9703a3
Firman Azhar Riyadi <33318547+FirmanAzharR@users.noreply.github.com>
2021-02-23 16:16:19 +0700

    fix address

--- src/views/YourCart.vue
+++ src/views/YourCart.vue
@@ -300,7 +300,7 @@ export default {
         )
       } else if (this.selected === '') {
         this.makeToast('Payment method ', 'Choose payment method', 'warning')
-      } else if (this.getProfile.user_address === 'NULL') {
+      } else if (this.getProfile.user_address == null) {
         this.makeToast(
           'Fill your address on profile',
           'Your address is empty',
